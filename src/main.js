f49e56677f9b3cfd889ba5e7040c2eceb1285c52
unknown <1910198192@qq.com>
2020-06-09 16:03:39 +0800

    '初始化项目'

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router/index'
+import store from './store/index'
+import VueLazyLoad from 'vue-lazyload'
 
 Vue.config.productionTip = false
 
+Vue.use(VueLazyLoad, {
+  preLoad: 1,
+  loading: require('assets/img/common/placeholder.png')
+})
+
 new Vue({
   render: h => h(App),
+  store,
+  router
 }).$mount('#app')
